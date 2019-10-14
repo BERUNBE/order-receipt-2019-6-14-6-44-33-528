39cab8648aa624937c39d594d0ac5d517ac4d71d
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-14 17:13:34 +0800

    Remove comments

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -1,11 +1,5 @@
 package org.katas.refactoring;
 
-/**
- * OrderReceipt prints the details of order including customer name, address, description, quantity,
- * price and amount. It also calculates the sales tax @ 10% and prints as part
- * of order. It computes the total order amount (amount of individual lineItems +
- * total sales tax) and prints it.
- */
 public class OrderReceipt {
     private Order order;
 
@@ -16,14 +10,11 @@ public class OrderReceipt {
     public String printReceipt() {
         StringBuilder output = new StringBuilder();
 
-        // print headers
         output.append("======Printing Orders======\n");
 
-        // print date, bill no, customer name
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
 
-        // prints lineItems
         double totSalesTx = 0d;
         double tot = 0d;
         for (LineItem lineItem : order.getLineItems()) {
@@ -32,18 +23,14 @@ public class OrderReceipt {
             output.append(lineItem.getQuantity()).append('\t');
             output.append(lineItem.getTotalAmount()).append('\n');
 
-            // calculate sales tax @ rate of 10%
             double salesTax = lineItem.getTotalAmount() * .10;
             totSalesTx += salesTax;
 
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
             tot += lineItem.getTotalAmount() + salesTax;
         }
 
-        // prints the state tax
         output.append("Sales Tax").append('\t').append(totSalesTx);
 
-        // print total amount
         output.append("Total Amount").append('\t').append(tot);
         return output.toString();
     }
