bc915c3d5e1fea626ff3b06c91919ef0d66b7148
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-15 08:01:00 +0800

    Extract get totalAmount to method

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -16,27 +16,32 @@ public class OrderReceipt {
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
 
-        double totalAmount = 0d;
         for (LineItem lineItem : order.getLineItems()) {
             output.append(lineItem.getDescription()).append('\t');
             output.append(lineItem.getPrice()).append('\t');
             output.append(lineItem.getQuantity()).append('\t');
             output.append(lineItem.getTotalAmount()).append('\n');
-
-            double salesTax = lineItem.getTotalAmount() * .10;
-            totalAmount += lineItem.getTotalAmount() + salesTax;
         }
 
-        output.append("Sales Tax").append('\t').append(getTotalSalesTax(order.getLineItems()));
-        output.append("Total Amount").append('\t').append(totalAmount);
+        double totalSalesTax = getTotalSalesTax();
+        double totalAmountWithTax = getTotalAmount() + totalSalesTax;
+        output.append("Sales Tax").append('\t').append(totalSalesTax);
+        output.append("Total Amount").append('\t').append(totalAmountWithTax);
 
         return output.toString();
     }
 
-    private double getTotalSalesTax(List<LineItem> lineItemList) {
-        return lineItemList.stream()
+    private double getTotalSalesTax() {
+        return order.getLineItems().stream()
                 .map(LineItem::getTotalAmount)
                 .map(subTotalAmount -> subTotalAmount * 0.10)
+                .reduce(Double::sum)
+                .orElse(0.0);
+    }
+
+    private double getTotalAmount() {
+        return order.getLineItems().stream()
+                .map(LineItem::getTotalAmount)
                 .reduce(Double::sum)
                 .orElse(0.0);
     }
